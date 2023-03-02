31b373cc749e6afb68f8d211c522d969e155a1e9
SIAM-AHMED-CP <118670054+SIAM-AHMED-CP@users.noreply.github.com>
2023-03-03 00:25:44 +0600

    Add files via upload

--- AJX.py
+++ AJX.py
@@ -3,8 +3,8 @@ arch=platform.architecture()[0]
 os.system("git pull")
 try:
 	if "32" in arch:
-		import c2
+		import a332
 	elif "64" in arch:
-		import c264
+		import a364
 except:
 	print("An error occured")
