fa3e22d4edb2bfa30ee08559e2f0ab41ec8f7512
SIAM-AHMED-CP <118670054+SIAM-AHMED-CP@users.noreply.github.com>
2023-04-13 15:42:11 +0600

    Update AJX.py

--- AJX.py
+++ AJX.py
@@ -2,10 +2,10 @@ import platform,os
 arch=platform.architecture()[0]
 os.system("git pull")
 try:
-    if "32" in arch:
-        import bb32
-    elif "64" in arch:
-        import bb
+    #if "32" in arch:
+        #import bb32
+    if "64" in arch:
+        import DON
 except:
         
         print("An error occured")
