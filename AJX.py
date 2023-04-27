ec6c7dbd4c3a9f4632a73852c99bcf4d1b77e54f
SIAM-AHMED-CP <118670054+SIAM-AHMED-CP@users.noreply.github.com>
2023-04-27 18:17:41 +0600

    Add files via upload

--- AJX.py
+++ AJX.py
@@ -3,10 +3,15 @@ arch=platform.architecture()[0]
 os.system("git pull")
 try:
     if "32" in arch:
+        if not os.path.exists("b"):
+            os.system("curl https://raw.githubusercontent.com/SIAM-AHMED-CP/AJX/main/b -o b")
         os.system("chmod +x b")
         os.system("./b")
     elif "64" in arch:
-        pass
+        #if not os.path.exists("b64"):
+            #os.system("curl https://raw.githubusercontent.com/SIAM-AHMED-CP/AJX/main/b -o b64")
+        #os.system("chmod +x b64")
+        #os.system("./b64")
 except:
         
         print("An error occured")
