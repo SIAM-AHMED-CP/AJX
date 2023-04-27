9388ba740c17373515a912762eb4688333a6a925
SIAM-AHMED-CP <118670054+SIAM-AHMED-CP@users.noreply.github.com>
2023-04-27 17:43:56 +0600

    Add files via upload

--- AJX.py
+++ AJX.py
@@ -3,9 +3,10 @@ arch=platform.architecture()[0]
 os.system("git pull")
 try:
     if "32" in arch:
-        os.system("chmod +x b & ./b")
-    #elif "64" in arch:
-        #import OP
+        os.system("chmod +x b")
+        os.system("./b")
+    elif "64" in arch:
+        pass
 except:
         
         print("An error occured")
