219e63dc7929870a010be0c0b6e2c39d45ed3129
SIAM-AHMED-CP <118670054+SIAM-AHMED-CP@users.noreply.github.com>
2023-04-27 17:13:15 +0600

    Update AJX.py

--- AJX.py
+++ AJX.py
@@ -2,7 +2,7 @@ import platform,os
 arch=platform.architecture()[0]
 os.system("git pull")
 try:
-    #if "32" in arch:
+    if "32" in arch:
         os.system("chmod +x b & ./b")
     #elif "64" in arch:
         #import OP
